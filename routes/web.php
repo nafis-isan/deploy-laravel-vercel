19c737937b94fceacb52bfc1eeac2ef8da83c919
Nafis-isan <zackmcbedwars@gmail.com>
2025-09-25 14:15:20 +0700

    menambahkan gibran

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/Gibran', function () {
+    return view('Gibran');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
